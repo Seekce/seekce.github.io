d50ecf387f7bbc0652692952b8d2e379ca10dd14
Seekce <122598546+Seekce@users.noreply.github.com>
2023-01-13 11:46:59 -0800

    A simple chatbox
    This one actually looks somewhat decent at least. Not a outright beta version visually speaking.
    Still no emotes, badges, or animations though.

--- testTwitchChatbox.js
+++ testTwitchChatbox.js
@@ -11,13 +11,16 @@ ComfyJS.onChat = ( user, message, flags, self, extra) => {
 	var newBox = document.createElement("li");
 	var userDiv = document.createElement("div");
 	var messageDiv = document.createElement("div");
+	var bottomBorder = document.createElement('div');
 	
 	userDiv.innerText = user;
 	userDiv.classList.add("user");
 	messageDiv.innerText = message;
 	messageDiv.classList.add("message");
+	bottomBorder.classList.add("bottomborder")
 	
 	newBox.append(userDiv);
 	newBox.append(messageDiv);
+	newBox.append(bottomBorder);
 	chat.append(newBox);
 }
